c30ab73d4b8df900918c4ce8b17fdf486b64cffc
ZhuElly <34325707+Elly2018@users.noreply.github.com>
2023-09-28 14:08:19 +0800

    Change the file structure

--- build/windows_codegen.bat
+++ build/windows_codegen.bat
@@ -1,3 +1,4 @@
 @echo off
+cd ..
 cmake -B GDExtensionTemplate-build -G"Visual Studio 17 2022"  -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=GDExtensionTemplate-install GDExtensionTemplate
 pause
