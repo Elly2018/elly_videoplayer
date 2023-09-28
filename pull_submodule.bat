ff24148f4d5765cc1876dad35e04b8aa459e5cce
ZhuElly <34325707+Elly2018@users.noreply.github.com>
2023-09-28 09:19:30 +0800

    Change that instead of using source code, using the share library ffmpeg official provide will be more effienct

--- pull_submodule.bat
+++ pull_submodule.bat
@@ -1,2 +1,6 @@
-git clone -b release/6.0 https://github.com/FFmpeg/FFmpeg.git
+@echo off
 git clone -b 4.1 https://github.com/godotengine/godot-cpp.git
+curl --output ffmpeg.zip -L -O https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl-shared.zip
+tar -xf ffmpeg.zip
+rename ffmpeg-master-latest-win64-gpl-shared ffmpeg
+pause
