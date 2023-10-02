4dacb378e6edb0ff435d810bb59bdf458a7bff4c
ZhuElly <34325707+Elly2018@users.noreply.github.com>
2023-10-02 09:38:56 +0800

    Added batch file quickly copy result to example folder

---
+++ ResultToExample.bat
@@ -0,0 +1,3 @@
+@echo off
+copy "GDExtensionTemplate-build\GDExtensionTemplate\lib\Windows-AMD64\Debug\gd_videoplayer-d.dll" "example\addons\videoplayer\lib\Windows-AMD64\gd_videoplayer-d.dll" /Y
+pause
