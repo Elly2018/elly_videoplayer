b540c44adc906be2e94900191e63f91e67062be4
ZhuElly <34325707+Elly2018@users.noreply.github.com>
2023-10-25 09:58:28 +0800

    Create a unity interface

---
+++ src/src/unity/UnityInterface.h
@@ -0,0 +1,18 @@
+/*
+* Basically, the interface is where unity c# script interpolate with.
+*/
+#pragma once
+#include "Unity/IUnityInterface.h"
+#include "Unity/IUnityGraphics.h"
+
+extern "C" UNITY_INTERFACE_EXPORT int UNITY_INTERFACE_API interfaceCreatePlayer();
+extern "C" UNITY_INTERFACE_EXPORT int UNITY_INTERFACE_API interfaceGetPlayerState(int id);
+extern "C" UNITY_INTERFACE_EXPORT void UNITY_INTERFACE_API interfaceDestroyPlayer(int id);
+extern "C" UNITY_INTERFACE_EXPORT void UNITY_INTERFACE_API interfacePlay(int id);
+extern "C" UNITY_INTERFACE_EXPORT void UNITY_INTERFACE_API interfacePause(int id);
+extern "C" UNITY_INTERFACE_EXPORT void UNITY_INTERFACE_API interfaceStop(int id);
+extern "C" UNITY_INTERFACE_EXPORT void UNITY_INTERFACE_API interfaceSeek(int id, double time);
+extern "C" UNITY_INTERFACE_EXPORT double UNITY_INTERFACE_API interfaceGetCurrentTime(int id);
+extern "C" UNITY_INTERFACE_EXPORT double UNITY_INTERFACE_API interfaceMediaLength(int id);
+extern "C" UNITY_INTERFACE_EXPORT void UNITY_INTERFACE_API interfaceLoadPath(int id, const char* path);
+extern "C" UNITY_INTERFACE_EXPORT void UNITY_INTERFACE_API interfaceLoadPathAsync(int id, const char* path);
