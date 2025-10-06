597dcb041aaf7ae2ca892f5d3ae85d6dc2ab0eed
ZhuElly <34325707+Elly2018@users.noreply.github.com>
2025-10-06 19:18:51 +0800

    Update

--- build/linux_pull_submodule.sh
+++ build/linux_pull_submodule.sh
@@ -3,6 +3,6 @@ cd ..
 git clone -b 4.1 https://github.com/godotengine/godot-cpp.git
 cd src
 curl --output ffmpeg.zip -L -O https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl-shared.zip
-tar -xf ffmpeg.zip
-rename ffmpeg-master-latest-win64-gpl-shared ffmpeg
+unzip ffmpeg.zip
+mv ffmpeg-master-latest-win64-gpl-shared ffmpeg
 rm ./ffmpeg.zip
