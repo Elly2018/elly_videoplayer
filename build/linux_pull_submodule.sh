a88c6e37edc1c411bded8febbe35dcd3c70ad2f0
ZhuElly <34325707+Elly2018@users.noreply.github.com>
2025-10-07 16:11:17 +0800

    Added windows program helper

--- build/linux_pull_submodule.sh
+++ build/linux_pull_submodule.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd ../lib
-curl --output ffmpeg.zip -L -O https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-linux64-gpl-shared.tar.xz
-unzip ffmpeg.zip
-mv ffmpeg-master-latest-win64-gpl-shared ffmpeg-linux64
-rm ./ffmpeg.zip
+curl --output ffmpeg.tar.xz -L -O https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-linux64-gpl-shared.tar.xz
+tar -xf ffmpeg.tar.xz
+mv ffmpeg-master-latest-linux64-gpl-shared ffmpeg-linux64
+rm ./ffmpeg.tar.xz
--- build/windows_pull_submodule.bat
+++ build/windows_pull_submodule.bat
@@ -1,7 +1,8 @@
 @echo off
+set path=%path%;%cd%
 cd ../lib
 curl --output ffmpeg.zip -L -O https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl-shared.zip
-tar -xf ffmpeg.zip
-rename ffmpeg-master-latest-win64-gpl-shared ffmpeg-win64
+7z x ffmpeg.zip -offmpeg-master-latest-win64-gpl-shared
+ren ffmpeg-master-latest-win64-gpl-shared ffmpeg-win64
 del ffmpeg.zip
 pause
